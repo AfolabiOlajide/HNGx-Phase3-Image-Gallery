7a0a2436972428e37eac6716a0d1ae0bac077af5
Afolabi Olajide Samuel <samolajide20@gmail.com>
2023-09-21 19:26:32 +0100

    error message on signin, built

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,10 +1,15 @@
 'use client'
 import { useRef } from "react"
 import { signIn } from "next-auth/react";
+import { useSearchParams } from 'next/navigation'
 // local
 import Input from "../components/Input";
 
 function SignIn() {
+    const searchParams = useSearchParams()
+    const errorParam = searchParams.get('error') 
+    console.log(errorParam)
+
     const emailRef = useRef<HTMLInputElement | null>(null);
     const passwordRef = useRef<HTMLInputElement | null>(null);
 
@@ -18,6 +23,12 @@ function SignIn() {
     }
     return (
         <div className=" flex flex-col gap-[2rem]">
+            {/* Error */}
+            { errorParam && 
+                <div className="error bg-red-600 text-white p-[1rem] rounded-md">
+                    <p>There was a problem signing you in, please try again with the right credentials.</p>
+                </div>
+            }
             <Input ref={emailRef} label="Email" name="email" type="email" />
             <Input ref={passwordRef} label="Password" name="password" type="password" />
             <button className="bg-grad p-[1rem] rounded-md" type="submit" onClick={handleSignIn}>
